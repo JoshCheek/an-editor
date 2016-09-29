b9d877e87ac648a4c0ecd702cb7c05dcf18c9a4c
Josh Cheek <josh.cheek@gmail.com>
2016-09-29 17:34:05 -0500

    Reformat the case statement

--- lib/editor.rb
+++ lib/editor.rb
@@ -30,14 +30,22 @@ class Editor
     stdout.print ansi.topleft, ansi.clear, state.to_s
     input = stdin.readpartial 1024
     case input
-    when ?\C-d then self.running = false
-    when ?\C-a then self.state = state.to_beginning_of_line
-    when ?\C-e then self.state = state.to_end_of_line
-    when ?\C-p, ansi.up_arrow   then self.state = state.cursor_up
-    when ?\C-n, ansi.down_arrow then self.state = state.cursor_down
-    when ?\C-b, ansi.left_arrow then self.state = state.cursor_left
-    when ?\C-f, ansi.right_arrow then self.state = state.cursor_right
-    else self.state = state.insert(input)
+    when ?\C-d
+      self.running = false
+    when ?\C-a
+      self.state = state.to_beginning_of_line
+    when ?\C-e
+      self.state = state.to_end_of_line
+    when ?\C-p, ansi.up_arrow
+      self.state = state.cursor_up
+    when ?\C-n, ansi.down_arrow
+      self.state = state.cursor_down
+    when ?\C-b, ansi.left_arrow
+      self.state = state.cursor_left
+    when ?\C-f, ansi.right_arrow
+      self.state = state.cursor_right
+    else
+      self.state = state.insert(input)
     end
     self
   end
